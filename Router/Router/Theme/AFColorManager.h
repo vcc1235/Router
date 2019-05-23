4fbcdbb752957879e07a8bba3745da5d41ac25c3
vcc1235 <1041497818@qq.com>
2019-05-23 18:12:07 +0800

    fdsafds

--- Router/Router/Theme/AFColorManager.h
+++ Router/Router/Theme/AFColorManager.h
@@ -1,5 +1,5 @@
 //
-//  AFThemeDelegate.h
+//  AFColorManager.h
 //  Router
 //
 //  Created by alete on 2019/5/23.
@@ -8,13 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-@protocol AFThemeDelegate <NSObject>
-/// 单例模式
-+(instancetype)vc_shareInstance ;
-/// 加载数据模型   json ///文件路径
--(void)vc_loadLocalJson:(NSString *)json ;
-/// 主题模式 默认
-@property (nonatomic, strong) NSString *vc_style ;
+NS_ASSUME_NONNULL_BEGIN
 
+@interface AFColorManager : NSObject
+
+/// 单例模式
++(instancetype)shareInstance ;
+/// 加载数据模型   json ///文件路径
+-(void)loadLocalJson:(NSString *)json ;
+/// 主题模式 默认
+@property (nonatomic, strong) NSString *style ;
 
 @end
+
+NS_ASSUME_NONNULL_END
